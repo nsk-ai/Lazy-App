e08b361db1332145beaeba4ef597d44bfe6a9cd2
Ohakim Nnaemeka <ohakim.nnaemeka@gmail.com>
2022-10-05 18:24:49 +0000

    added the upload file capability

--- streamlit/pages/Upload_to_Transcribe.py
+++ streamlit/pages/Upload_to_Transcribe.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import whisper
-import numpy as np
+# import numpy as np
+import torchaudio
+from io import BytesIO
 #from pydub import AudioSegment
 
 
@@ -8,14 +10,22 @@ def run_uploads():
     audio_input = st.file_uploader("Upload audio here:", type = ["mp3","m4a","mp4","wav"])
 
     if audio_input is not None:
-        audio_ = audio_input.read()
+        audio_ = BytesIO(audio_input.read())
+        waveform, _ = torchaudio.load(audio_)
 
     output = ""
     with st.spinner("Transcribing in progress..."):
         if st.button("Transcribe"):
             model = whisper.load_model("base")
-            result = model.transcribe(audio_)
-            output = result["text"]
+            # result = model.transcribe(waveform)
+            # output = result["text"]
+            audio = whisper.pad_or_trim(waveform.flatten()).to(model.device)
+            mel = whisper.log_mel_spectrogram(audio)
+            
+            # decode the audio
+            options = whisper.DecodingOptions(fp16 = False,language="en", without_timestamps=True)
+            result = model.decode(mel, options)
+            output = result.text
 
     return st.text_area("",output, placeholder = "Transcribed text...")
 
